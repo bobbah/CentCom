049964297df911494bd16d3cdb1360893088514c
bobbahbrown <bobbahbrown@gmail.com>
2023-07-09 01:34:04 -0300

    Remove unused packages

--- CentCom.API/Models/AppVersionDTO.cs
+++ CentCom.API/Models/AppVersionDTO.cs
@@ -7,6 +7,7 @@ namespace CentCom.API.Models;
 /// </summary>
 /// <param name="Version">The version of the application in semver</param>
 /// <param name="Commit">The commit hash of the build that the application is running</param>
+/// <param name="CopyrightNotice">The copyright notice for this version of the application</param>
 public record AppVersionDTO(string Version, string Commit, string CopyrightNotice)
 {
     internal AppVersionDTO(AssemblyInformation assemblyInfo) : this(assemblyInfo.Version, assemblyInfo.Commit,
